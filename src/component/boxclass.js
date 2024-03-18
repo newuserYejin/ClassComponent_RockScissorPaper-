b089650cbc2693822fdfea2b2d98988e837815d2
jin <jin200126@gmail.com>
2024-03-19 02:13:38 +0900

    second upload

--- src/component/boxclass.js
+++ src/component/boxclass.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class boxclass extends Component {
     render() {
         return (
-            <div className='box'>
+            <div className={`box ${this.props.result}`}>
                 <h1>{this.props.title}</h1>
                 <img className='item-img' src={this.props.item && this.props.item.img}></img>  {/* 가드값 설정 */}
                 <h2>{this.props.result}</h2>
